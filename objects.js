3b2f5fa105f2e35bb2465c4295eb36efe820e6cb
alinamva <alinamvaa@mail.ru>
2023-10-03 19:42:02 +0400

    new tasks

--- objects.js
+++ objects.js
@@ -147,3 +147,72 @@
 // };
 
 // applyMondayBonus(scoreBoard);
+
+// function createVisitor(name, age, ticketId) {
+//   let obj = {};
+
+//   obj["name"] = name;
+//   obj["age"] = age;
+//   obj["ticketId"] = ticketId;
+//   return obj;
+// }
+
+// console.log(createVisitor("Alina", "21", "AJS26"));
+
+// function revokeTicket(visitor) {
+//   visitor.ticketId = null;
+//   return visitor;
+// }
+
+// const visitor = {
+//   name: "Verena Nardi",
+//   age: 45,
+//   ticketId: "H32AZ123",
+// };
+
+// console.log(revokeTicket(visitor));
+
+// function ticketStatus(tickets, ticketId) {
+//   if (tickets[ticketId] === null) {
+//     return "not sold";
+//   } else if (tickets[ticketId]) {
+//     return `sold to ${tickets[ticketId]}`;
+//   } else {
+//     return "unknown ticket id";
+//   }
+// }
+
+// const tickets = {
+//   "0H2AZ123": null,
+//   "23LA9T41": "Verena Nardi",
+// };
+// console.log(ticketStatus(tickets, "0H2AZ123"));
+// console.log(ticketStatus(tickets, "23LA9T41"));
+// console.log(ticketStatus(tickets, "RE90VAW7"));
+
+function gtcVersion(visitor) {
+  if (visitor.gtc?.signed === true) {
+    return visitor.gtc.version;
+  } else {
+    return undefined;
+  }
+}
+
+const visitorNew = {
+  name: "Verena Nardi",
+  age: 45,
+  ticketId: "H32AZ123",
+  gtc: {
+    signed: true,
+    version: "2.1",
+  },
+};
+const visitorOld = {
+  name: "Verena Nardi",
+  age: 45,
+  ticketId: "H32AZ123",
+};
+
+console.log(gtcVersion(visitorOld));
+
+console.log(gtcVersion(visitorNew));
